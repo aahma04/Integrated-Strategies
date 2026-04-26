8b31b2618fbf549eb226e3bf24fe6dcb110a510d
Affan Ahmad <affanahmad2004@gmail.com>
2026-04-26 01:15:08 -0400

    Fixed AudioHandler to not duplicate on level change, but delete on scene switch

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -4,9 +4,18 @@ using UnityEngine.SceneManagement;
 public class AudioHandler : MonoBehaviour
 {
 
+    private static AudioHandler instance;
     void Start()
     {
-        DontDestroyOnLoad(this);
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
