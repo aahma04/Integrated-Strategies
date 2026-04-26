fc5f35584a014f73739e836a85932580b009662e
Affan Ahmad <affanahmad2004@gmail.com>
2026-04-26 01:25:46 -0400

    Fixed AudioHandler.cs AGAIN

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -3,26 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class AudioHandler : MonoBehaviour
 {
-
     private static AudioHandler instance;
-    void Start()
+
+    void Awake()
     {
         if (instance != null)
         {
-            Destroy(instance);
+            Destroy(gameObject);
+            return;
         }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
+    void OnEnable()
     {
-        if (scene.name != "Level") {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Level")
+        {
             Destroy(gameObject);
+            instance = null;
         }
     }
-    
 }
